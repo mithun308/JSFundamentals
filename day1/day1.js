420132501fde321cacab6c320970eb44ffdfe0b8
mithun308 <mithu.ashwath@gmail.com>
2026-03-30 06:01:32 +0530

    Execution Context, Call Stack, Hosting

---
+++ day1/day1.js
@@ -0,0 +1,18 @@
+var n=2;
+function square(num){
+    var ans=num*num;
+    return ans
+}
+var square2=square(n)
+var square4=square(4)
+console.log(square2)
+console.log(square4)
+
+getName()
+console.log(x)
+console.log(getName)
+
+var x=7
+function getName(){
+    console.log("Namaste javascript")
+}
